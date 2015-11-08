e6bcaddafd940db6fa6fe2f92c60e5abe82ff63a
YeongChai-07 <butchercai@gmail.com>
2015-11-09 04:35:45 +0800

    ADDED codes to include panels on each of the possible routes for the displaying of the step by step route directions. FIXED: The collapsibility of each of the possible routes button - previously after viewing the route directions details by pressing the button once, pressing the button again WILL NOT hide the route directions deatils.

--- Final Submission/public/modules/core/controllers/home.client.controller.js
+++ Final Submission/public/modules/core/controllers/home.client.controller.js
@@ -93,11 +93,6 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
     }
 ]).controller("GMapController", ['$scope','$mdDialog','$anchorScroll',
     function($scope,$mdDialog,$anchorScroll){
-        $scope.goToSteps = function(){
-            $location.hash('routeInstruction');
-            $anchorScroll();
-        };
-        //Scroll to Route directions at the bottom of page
         /*
          In order to display a map, we have to first declare a variable mapOptions.
          mapOptions will help us define many parameters related to the map
@@ -141,23 +136,10 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
          */
 
         /* Route and display function found at Google Map API */
-        $scope.toggleCollapsed = {collapsed:1,buttonCollapsed:1};
+        //$scope.toggleCollapsed = {collapsed:1,buttonCollapsed:1};
         $scope.data = {vehicles:'any'};
 
         $scope.routeToLocation = function(location,endingpoint) {
-            var vehiclesInRoute1 = [];
-            var vehiclesInRoute2 = [];
-            var vehiclesInRoute3 = [];
-            var vehiclesInRoute4 = [];
-
-            var transportType1='';
-            var transportType2='';
-            var transportType3='';
-            var transportType4='';
-
-            var routeDataStruct = [];
-            $scope.routeButtons = routeDataStruct;
-
             directionsDisplay.setDirections({routes: []});
             if($scope.data.vehicles=='bus'){
                 var request = {
@@ -192,128 +174,32 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
             directionsService.route(request, function(response, status) {
                 if (status == google.maps.DirectionsStatus.OK) {
                     console.log(response);
-                    directionsDisplay.setDirections(response);
-                    directionsDisplay.setMap($scope.map);
-                    directionsDisplay.setPanel(document.getElementById('routeInstruction'));
-                    //$scope.toggleCollapsed.buttonCollapsed = 0;
                     var collapsedAttribute = [];
                     console.log(response.routes[0].legs[0].steps.length);
-                    for(var j=0;j<4;j++){
-                        for(var i=0;i<response.routes[j].legs[0].steps.length;i++){
-                            if(response.routes[j].legs[0].steps[i].travel_mode!="WALKING"){
-                                if(j==0){
-                                    vehiclesInRoute1.push(response.routes[j].legs[0].steps[i].transit.line.vehicle.name);
-                                }
-                                else if(j==1){
-                                    vehiclesInRoute2.push(response.routes[j].legs[0].steps[i].transit.line.vehicle.name);
-                                }
-                                else if(j==2){
-                                    vehiclesInRoute3.push(response.routes[j].legs[0].steps[i].transit.line.vehicle.name);
-                                }
-                                else{
-                                    vehiclesInRoute4.push(response.routes[j].legs[0].steps[i].transit.line.vehicle.name);
-                                }
-                            }
+					var vehiclesInRoute = [];
+                    for(var i=0;i<response.routes[0].legs[0].steps.length;i++){
+                        if(response.routes[0].legs[0].steps[i].travel_mode!="WALKING"){
+                            vehiclesInRoute.push(response.routes[0].legs[0].steps[i].transit.line.vehicle.name);
                         }
-                    }
-                    //Determining transport type for each routes
-                    if(vehiclesInRoute1.indexOf("Subway")==-1 && vehiclesInRoute1.indexOf("Bus")==-1){
-                        transportType1 = 'Walk there!';
-                    }
-                    else if(vehiclesInRoute1.indexOf("Bus")==-1){
-                        transportType1 = 'Train only';
-                    }
-                    else if(vehiclesInRoute1.indexOf("Bus")!=-1 && vehiclesInRoute1.indexOf("Subway")!=-1){
-                        transportType1 = 'Bus and Train';
-                    }
-                    else if(vehiclesInRoute1.indexOf("Subway")==-1){
-                        transportType1 = 'Bus only';
-                    }
-                    if(vehiclesInRoute2.indexOf("Subway")==-1 && vehiclesInRoute2.indexOf("Bus")==-1){
-                        transportType2 = 'Walk there!';
-                    }
-                    else if(vehiclesInRoute2.indexOf("Bus")==-1){
-                        transportType2 = 'Train only';
-                    }
-                    else if(vehiclesInRoute2.indexOf("Bus")!=-1 && vehiclesInRoute2.indexOf("Subway")!=-1){
-                        transportType2 = 'Bus and Train';
-                    }
-                    else if(vehiclesInRoute2.indexOf("Subway")==-1){
-                        transportType2 = 'Bus only';
-                    }
-                    if(vehiclesInRoute3.indexOf("Subway")==-1 && vehiclesInRoute3.indexOf("Bus")==-1){
-                        transportType3 = 'Walk there!';
-                    }
-                    else if(vehiclesInRoute3.indexOf("Bus")==-1){
-                        transportType3 = 'Train only';
-                    }
-                    else if(vehiclesInRoute3.indexOf("Bus")!=-1 && vehiclesInRoute3.indexOf("Subway")!=-1){
-                        transportType3 = 'Bus and Train';
-                    }
-                    else if(vehiclesInRoute3.indexOf("Subway")==-1){
-                        transportType3 = 'Bus only';
-                    }
-                    if(vehiclesInRoute4.indexOf("Subway")==-1 && vehiclesInRoute4.indexOf("Bus")==-1){
-                        transportType4 = 'Walk there!';
-                    }
-                    else if(vehiclesInRoute4.indexOf("Bus")==-1){
-                        transportType4 = 'Train only';
-                    }
-                    else if(vehiclesInRoute4.indexOf("Bus")!=-1 && vehiclesInRoute4.indexOf("Subway")!=-1){
-                        transportType4 = 'Bus and Train';
-                    }
-                    else if(vehiclesInRoute4.indexOf("Subway")==-1){
-                        transportType4 = 'Bus only';
-                    }
+					}
+					
                     //Save the current instance of response (DirectionsResult) to $scope
                     // for use in later of the code
                     $scope.directionsResult = response;
 
+					var routeDataStruct = [];
                     for(var count=0;count<response.routes.length;count++)
                     {
-                        // Initialize buttonCollapsed to 0 (FALSE) and collapsed to 1 (TRUE)
+						 // Initialize buttonCollapsed to 0 (FALSE) and collapsed to 1 (TRUE)
                         collapsedAttribute.push({buttonCollapsed: 0,
                             collapsed: 1});
-                        if(count==0){
-                            routeDataStruct.push({routeID: (count),
-                                routeDistance: ((response.routes[count].legs[0].distance.value)/1000).toFixed(0) + " km ",
-                                routeDuration: ((response.routes[count].legs[0].duration.value)/60).toFixed(0) + " minutes ",
-                                transportType: transportType1,
-                                buttonCollapsedAttr: "toggleCollapsed.buttonCollapsed[" + count + "]",
-                                clickCollapsed: "toggleCollapsed.collapsed[" + count + "]=!toggleCollapsed.collapsed[" + count + "]",
-                                divCollapsedAttr: "toggleCollapsed.collapsed[" + count + "]",
-                                divInstructionID: "routeInstruction" + count});
-                        }
-                        else if(count==1){
-                            routeDataStruct.push({routeID: (count),
-                                routeDistance: ((response.routes[count].legs[0].distance.value)/1000).toFixed(0) + " km ",
-                                routeDuration: ((response.routes[count].legs[0].duration.value)/60).toFixed(0) + " minutes ",
-                                transportType: transportType2,
-                                buttonCollapsedAttr: "toggleCollapsed.buttonCollapsed[" + count + "]",
-                                clickCollapsed: "toggleCollapsed.collapsed[" + count + "]=!toggleCollapsed.collapsed[" + count + "]",
-                                divCollapsedAttr: "toggleCollapsed.collapsed[" + count + "]",
-                                divInstructionID: "routeInstruction" + count});
-                        }
-                        else if(count==2){
-                            routeDataStruct.push({routeID: (count),
-                                routeDistance: ((response.routes[count].legs[0].distance.value)/1000).toFixed(0) + " km ",
-                                routeDuration: ((response.routes[count].legs[0].duration.value)/60).toFixed(0) + " minutes ",
-                                transportType: transportType3,
-                                buttonCollapsedAttr: "toggleCollapsed.buttonCollapsed[" + count + "]",
-                                clickCollapsed: "toggleCollapsed.collapsed[" + count + "]=!toggleCollapsed.collapsed[" + count + "]",
-                                divCollapsedAttr: "toggleCollapsed.collapsed[" + count + "]",
-                                divInstructionID: "routeInstruction" + count});
-                        }
-                        else if(count==3){
-                            routeDataStruct.push({routeID: (count),
-                                routeDistance: ((response.routes[count].legs[0].distance.value)/1000).toFixed(0) + " km ",
-                                routeDuration: ((response.routes[count].legs[0].duration.value)/60).toFixed(0) + " minutes ",
-                                transportType: transportType4,
-                                buttonCollapsedAttr: "toggleCollapsed.buttonCollapsed[" + count + "]",
-                                clickCollapsed: "toggleCollapsed.collapsed[" + count + "]=!toggleCollapsed.collapsed[" + count + "]",
-                                divCollapsedAttr: "toggleCollapsed.collapsed[" + count + "]",
-                                divInstructionID: "routeInstruction" + count});
-                        }
+                        routeDataStruct.push({routeID: (count),
+                            routeDistance: ((response.routes[count].legs[0].distance.value)/1000).toFixed(0) + " km ",
+                            routeDuration: ((response.routes[count].legs[0].duration.value)/60).toFixed(0) + " minutes ",
+                            buttonCollapsedAttr: "toggleCollapsed[" + count + "].buttonCollapsed",
+                            clickCollapsed: "redisplayRoute(" + count  + ")",
+                            divCollapsedAttr: "toggleCollapsed[" + count + "].collapsed",
+                            divInstructionID: "routeInstruction" + count});
                     }
 
                     $scope.routeButtons = routeDataStruct;
@@ -337,10 +223,13 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
         } // end of routeToLocation function
 
         $scope.redisplayRoute = function(index){
-            directionsDisplay.setRouteIndex(index);
-            directionsDisplay.setDirections(response);
+			// Code logic to control on the ng-hide goes here
+			$scope.toggleCollapsed[index].collapsed=! $scope.toggleCollapsed[index].collapsed;
+			
+            directionsDisplay.setDirections($scope.directionsResult);
+			directionsDisplay.setRouteIndex(index);
             directionsDisplay.setMap($scope.map);
-            directionsDisplay.setPanel(document.getElementById('routeInstruction'));
+            directionsDisplay.setPanel(document.getElementById('routeInstruction' + index));
         }
     } // end of controller function
 ]);
